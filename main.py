b9f2cbada93cf88affed290daf7379747565115f
root <root@ip-172-31-23-115.ec2.internal>
2024-05-01 22:15:54 +0000

    atulizar

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from flask import Flask, jsonify, request, render_template
 from pymongo import MongoClient
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
 # Conexão com MongoDB
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://mongo:27017/')
 db = client['meubanco']
 collection = db['meucollection']
 
 @app.route('/add', methods=['POST'])
 def add_item():
-    item = request.json
-    collection.insert_one(item)
-    return jsonify({'status': 'Item adicionado com sucesso!'}), 201
+    app.logger.debug('Received request: %s', request.data)
+    try:
+        item = request.json
+        app.logger.debug('Parsed JSON: %s', item)
+        collection.insert_one(item)
+        return jsonify({'status': 'Item adicionado com sucesso!'}), 201
+    except Exception as e:
+        app.logger.error('An error occurred: %s', e)
+        raise
 
 
 @app.route('/list', methods=['GET'])
@@ -26,5 +34,5 @@ def quick_test():
     return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
